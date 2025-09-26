6113eb329a88822b197bb58d31afb61b27510d70
Gabriel Muraro <gabriel.muraro@starya.ai>
2025-09-26 00:52:34 -0300

    Add initial knowledge base with profiles and questions

---
+++ base_conhecimento.pl
@@ -0,0 +1,43 @@
+triha(full_stack, 'Desenvolvimento completo, englobando front-end e back-end').
+triha(hardware, 'Desenvolvimento de componentes físicos e infraestrutura dos computadores').
+triha(segurança_da_informação, 'Proteção de dados, redes e sistemas contra ataques').
+triha(ciência_de_dados, 'Análise de dados, aprendizado de máquina').
+triha(redes, 'Administração de servidores, cloud computing e conectividade').
+
+perfil(full_stack, logica_de_programação, 5).
+perfil(full_stack, concerto_de_maquinas, 1).
+perfil(full_stack, matemática_estatistica, 3).
+perfil(full_stack, design_visual, 4).
+perfil(full_stack, atendimento_ao_cliente, 2).
+
+perfil(hardware, logica_de_programação, 3).
+perfil(hardware, concerto_de_maquinas, 5).
+perfil(hardware, matemática_estatistica, 2).
+perfil(hardware, design_visual, 1).
+perfil(hardware, atendimento_ao_cliente, 4).
+
+perfil(segurança_da_informação, logica_de_programação, 4).
+perfil(segurança_da_informação, concerto_de_maquinas, 1).
+perfil(segurança_da_informação, matemática_estatistica, 5).
+perfil(segurança_da_informação, design_visual, 2).
+perfil(segurança_da_informação, atendimento_ao_cliente, 3).
+
+perfil(ciência_de_dados, logica_de_programação, 4).
+perfil(ciência_de_dados, concerto_de_maquinas, 1).
+perfil(ciência_de_dados, matemática_estatistica, 5).
+perfil(ciência_de_dados, design_visual, 2).
+perfil(ciência_de_dados, atendimento_ao_cliente, 3).
+
+perfil(redes, logica_de_programação, 4).
+perfil(redes, concerto_de_maquinas, 2).
+perfil(redes, matemática_estatistica, 3).
+perfil(redes, design_visual, 1).
+perfil(redes, atendimento_ao_cliente, 5).
+
+pergunta(1, 'Você tem afinidade com logica de programação?', logica_de_programação).
+pergunta(2, 'Você tem afinidade com concerto de maquinas?', concerto_de_maquinas).
+pergunta(3, 'Você tem afinidade com matemática e estatística?',matemática_estatistica).
+pergunta(4, 'Você tem afinidade com design visual?',design_visual).
+pergunta(5, 'Você tem afinidade com atendimento ao cliente?', atendimento_ao_cliente).
+
+
