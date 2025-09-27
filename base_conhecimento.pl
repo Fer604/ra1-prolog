f537ed7d31517d94dc5d554edea4fabd0a011a89
Fer604 <166722929+Fer604@users.noreply.github.com>
2025-09-26 22:10:57 -0300

    Update base_conhecimento.pl
    só removi os acentos

--- base_conhecimento.pl
+++ base_conhecimento.pl
@@ -1,43 +1,44 @@
-triha(full_stack, 'Desenvolvimento completo, englobando front-end e back-end').
-triha(hardware, 'Desenvolvimento de componentes físicos e infraestrutura dos computadores').
-triha(segurança_da_informação, 'Proteção de dados, redes e sistemas contra ataques').
-triha(ciência_de_dados, 'Análise de dados, aprendizado de máquina').
-triha(redes, 'Administração de servidores, cloud computing e conectividade').
+% --- Trilhas ---
+trilha(full_stack, 'Desenvolvimento completo, englobando front-end e back-end').
+trilha(hardware, 'Desenvolvimento de componentes físicos e infraestrutura dos computadores').
+trilha(seguranca_da_informacao, 'Proteção de dados, redes e sistemas contra ataques').
+trilha(ciencia_de_dados, 'Análise de dados, aprendizado de máquina').
+trilha(redes, 'Administração de servidores, cloud computing e conectividade').
 
-perfil(full_stack, logica_de_programação, 5).
-perfil(full_stack, concerto_de_maquinas, 1).
-perfil(full_stack, matemática_estatistica, 3).
+% --- Perfis ---
+perfil(full_stack, logica_programacao, 5).
+perfil(full_stack, concerto_maquinas, 1).
+perfil(full_stack, matematica_estatistica, 3).
 perfil(full_stack, design_visual, 4).
-perfil(full_stack, atendimento_ao_cliente, 2).
+perfil(full_stack, atendimento_cliente, 2).
 
-perfil(hardware, logica_de_programação, 3).
-perfil(hardware, concerto_de_maquinas, 5).
-perfil(hardware, matemática_estatistica, 2).
+perfil(hardware, logica_programacao, 3).
+perfil(hardware, concerto_maquinas, 5).
+perfil(hardware, matematica_estatistica, 2).
 perfil(hardware, design_visual, 1).
-perfil(hardware, atendimento_ao_cliente, 4).
+perfil(hardware, atendimento_cliente, 4).
 
-perfil(segurança_da_informação, logica_de_programação, 4).
-perfil(segurança_da_informação, concerto_de_maquinas, 1).
-perfil(segurança_da_informação, matemática_estatistica, 5).
-perfil(segurança_da_informação, design_visual, 2).
-perfil(segurança_da_informação, atendimento_ao_cliente, 3).
+perfil(seguranca_da_informacao, logica_programacao, 4).
+perfil(seguranca_da_informacao, concerto_maquinas, 1).
+perfil(seguranca_da_informacao, matematica_estatistica, 5).
+perfil(seguranca_da_informacao, design_visual, 2).
+perfil(seguranca_da_informacao, atendimento_cliente, 3).
 
-perfil(ciência_de_dados, logica_de_programação, 4).
-perfil(ciência_de_dados, concerto_de_maquinas, 1).
-perfil(ciência_de_dados, matemática_estatistica, 5).
-perfil(ciência_de_dados, design_visual, 2).
-perfil(ciência_de_dados, atendimento_ao_cliente, 3).
+perfil(ciencia_de_dados, logica_programacao, 4).
+perfil(ciencia_de_dados, concerto_maquinas, 1).
+perfil(ciencia_de_dados, matematica_estatistica, 5).
+perfil(ciencia_de_dados, design_visual, 2).
+perfil(ciencia_de_dados, atendimento_cliente, 3).
 
-perfil(redes, logica_de_programação, 4).
-perfil(redes, concerto_de_maquinas, 2).
-perfil(redes, matemática_estatistica, 3).
+perfil(redes, logica_programacao, 4).
+perfil(redes, concerto_maquinas, 2).
+perfil(redes, matematica_estatistica, 3).
 perfil(redes, design_visual, 1).
-perfil(redes, atendimento_ao_cliente, 5).
+perfil(redes, atendimento_cliente, 5).
 
-pergunta(1, 'Você tem afinidade com logica de programação?', logica_de_programação).
-pergunta(2, 'Você tem afinidade com concerto de maquinas?', concerto_de_maquinas).
-pergunta(3, 'Você tem afinidade com matemática e estatística?',matemática_estatistica).
-pergunta(4, 'Você tem afinidade com design visual?',design_visual).
-pergunta(5, 'Você tem afinidade com atendimento ao cliente?', atendimento_ao_cliente).
-
-
+% --- Perguntas ---
+pergunta(1, 'Você tem afinidade com lógica de programação?', logica_programacao).
+pergunta(2, 'Você tem afinidade com conserto de máquinas?', concerto_maquinas).
+pergunta(3, 'Você tem afinidade com matemática e estatística?', matematica_estatistica).
+pergunta(4, 'Você tem afinidade com design visual?', design_visual).
+pergunta(5, 'Você tem afinidade com atendimento ao cliente?', atendimento_cliente).
