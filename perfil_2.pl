4a7195642fd2645f669b580e808e7cb28319b4f2
DJoverdant <166723698+DJoverdant@users.noreply.github.com>
2025-09-27 23:42:12 -0300

    perfil 2

---
+++ perfil_2.pl
@@ -0,0 +1,6 @@
+% Perfil: gosta de hardware e conserto de máquinas
+resposta(1, n, logica_programacao).
+resposta(2, s, concerto_maquinas).
+resposta(3, n, matematica_estatistica).
+resposta(4, n, design_visual).
+resposta(5, s, atendimento_cliente).
