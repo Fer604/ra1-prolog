1d3ee2264df04d4b46050618cb0e9fbe26ff3ad6
DJoverdant <166723698+DJoverdant@users.noreply.github.com>
2025-09-27 23:41:37 -0300

    perfil 1

---
+++ perfil_1.pl
@@ -0,0 +1,6 @@
+% Perfil: gosta de lógica e matemática (ideal para ciência de dados ou segurança)
+resposta(1, s, logica_programacao).
+resposta(2, n, concerto_maquinas).
+resposta(3, s, matematica_estatistica).
+resposta(4, n, design_visual).
+resposta(5, n, atendimento_cliente).
