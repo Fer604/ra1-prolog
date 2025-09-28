ba2f6cfd13dc240f9cda97f9c3847074913aaa29
DJoverdant <166723698+DJoverdant@users.noreply.github.com>
2025-09-27 23:42:32 -0300

    perfil 3

---
+++ perfil_3.pl
@@ -0,0 +1,6 @@
+% Perfil: criativo e voltado a design (ideal para full_stack ou front-end)
+resposta(1, s, logica_programacao).
+resposta(2, n, concerto_maquinas).
+resposta(3, n, matematica_estatistica).
+resposta(4, s, design_visual).
+resposta(5, s, atendimento_cliente).
